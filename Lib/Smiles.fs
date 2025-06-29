be63c005491f02d3082851ae9660b1ff2361d40f
bitstormFA <bitstorm@tutanota.com>
2025-06-29 16:22:59 +0200

    Added handling of ez isomers

--- Lib/Smiles.fs
+++ Lib/Smiles.fs
@@ -19,9 +19,18 @@ type ParseState = {
    mutable mol: MolGraph
    mutable currentBondType: BondType option
    mutable connectID: NodeID option
+   mutable ez: int Option
    }
    with
-    static member Default = { branches=Stack(); ringConnect=Dictionary(); mol=Graph.Empty; currentBondType = None; connectID = None}
+    static member Default =
+                              {
+                                branches=Stack()
+                                ringConnect=Dictionary()
+                                mol=Graph.Empty
+                                currentBondType = None
+                                connectID = None
+                                ez = None 
+                                }
     member this.setCurrentBond b = this.currentBondType <- b
     member this.getCurrentBond: Bond = match this.currentBondType with
                                            | None -> Bond.fromBondType BondType.Single
@@ -31,7 +40,15 @@ type ParseState = {
         | Some connectTo ->
             let connectAtom = getNodeData connectTo this.mol
             let bondType = if this.currentBondType.IsNone && connectAtom.Value.IsAromatic && a.IsAromatic then BondType.Aromatic else this.getCurrentBond.Type
-            let newMol, newItems = (addNodeToNode a connectTo (Bond.fromBondType bondType) this.mol)
+            let newMol, newItems = if this.ez.IsNone then
+                                       (addNodeToNode a connectTo (Bond.fromBondType bondType) this.mol)
+                                   else
+                                       let direction = match this.ez with
+                                                        | Some 1 -> BondDirection.CisTrans1
+                                                        | Some 2 -> BondDirection.CisTrans2
+                                                        | _ -> BondDirection.NoDirection
+                                       (addNodeToNode a connectTo {Bond.Type = bondType; Conjugated=false; Direction=direction} this.mol)
+                
             this.mol <- newMol
             match newItems with
             | Some (_, newNodeID) -> this.connectID <- Some newNodeID; this.currentBondType <- None; Some newNodeID
@@ -122,10 +139,15 @@ let changeState2  f p =
     p >>= fun x -> updateUserState (f x) >>% x
     
 let bonds = %List.ofSeq(bondMap.Keys)
-let bond: Parser<unit, ParseState> = %% +. bonds -%> (fun bk -> bondMap[bk])
+let bond: Parser<unit, ParseState> = %% +. bonds -%> (fun bk -> bk, bondMap[bk])
                                      |> changeState (fun bt state ->
-                                         state.currentBondType <- Some bt
+                                         state.currentBondType <- Some (snd bt)
+                                         match fst bt with
+                                         | "/" -> state.ez <- Some 1
+                                         | @"\" -> state.ez <- Some 2
+                                         | _ -> state.ez <- None
                                          state)
+                                     
 
 let ringID: Parser<unit, ParseState> = digit |>> int <|>  %% %'%' -- +. digit -- +. digit -%>( fun d1 d2-> int(d1+d2))
                                        |> changeState (fun id state ->
